8a7ba5546d7a96123e3987fc5422e62880dbee0b
F5OEO <evaristec@gmail.com>
2018-11-01 15:25:37 +0000

    Add ssb using csdr

--- install.sh
+++ install.sh
@@ -2,7 +2,13 @@
 echo Install rpitx - some package need internet connexion -
 sudo apt-get update
 sudo apt-get install -y libsndfile1-dev git
-sudo apt-get install -y imagemagick
+sudo apt-get install -y imagemagick libfftw3-dev
+# We use CSDR as a dsp for analogs modes thanks to HA7ILM
+git clone https://github.com/simonyiszk/csdr
+cd csdr
+make && sudo make install
+cd ../
+
 cd src
 git clone https://github.com/F5OEO/librpitx
 cd librpitx/src
