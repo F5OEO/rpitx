0d0afb14193ddfe989ac03d2cdafe792a8a74d68
F5OEO <evaristec@gmail.com>
2019-01-08 14:57:12 +0000

    Put output at each time on sv1afnfilter

--- sv1afnfilter.sh
+++ sv1afnfilter.sh
@@ -2,8 +2,10 @@
 
 #COMMAND A BANDPATH FILTER FROM SV1AFN
 
+Init()
+{
 #GPIO Declaration
-if [ ! -f /sys/class/gpio/gpio26 ]; then
+if [ ! -d /sys/class/gpio/gpio26 ]; then
 echo "Create GPIOs"
 
 echo GPIO declaration
@@ -11,6 +13,7 @@ echo 26 > /sys/class/gpio/export
 echo 19 > /sys/class/gpio/export
 echo 13 > /sys/class/gpio/export
 echo 6  > /sys/class/gpio/export
+echo 21  > /sys/class/gpio/export
 
 
 #GPIO out
@@ -19,33 +22,54 @@ echo out > /sys/class/gpio/gpio26/direction
 echo out > /sys/class/gpio/gpio19/direction
 echo out > /sys/class/gpio/gpio13/direction
 echo out > /sys/class/gpio/gpio6/direction
-
+echo out > /sys/class/gpio/gpio21/direction
 
 fi
+}
 
 #Initialization : All at zero (disable filter)
 reset_all()
 {
+	#GPIO out
+echo GPIO Mode OUT
+echo out > /sys/class/gpio/gpio26/direction
+echo out > /sys/class/gpio/gpio19/direction
+echo out > /sys/class/gpio/gpio13/direction
+echo out > /sys/class/gpio/gpio6/direction
+echo out > /sys/class/gpio/gpio21/direction
 	echo 0 > /sys/class/gpio/gpio26/value
 	echo 0 > /sys/class/gpio/gpio19/value
 	echo 0 > /sys/class/gpio/gpio13/value
 	echo 0 > /sys/class/gpio/gpio6/value
 }
 
-reset_all
+Init
+
 case "$1" in
 "10m")
+    reset_all
 	echo 1 > /sys/class/gpio/gpio26/value
         ;;
 "15m")
+    reset_all
     echo 1 > /sys/class/gpio/gpio19/value
     ;;
 "20m")
+    reset_all
     echo 1 > /sys/class/gpio/gpio13/value
     ;;
 "40m")
+    reset_all
     echo 1 > /sys/class/gpio/gpio6/value
     ;;
+"ptton")
+    echo out > /sys/class/gpio/gpio21/direction
+    echo 1 > /sys/class/gpio/gpio21/value
+    ;;
+"pttoff")
+
+    echo in > /sys/class/gpio/gpio21/direction
+    ;;          
 
 *)
      ;;
